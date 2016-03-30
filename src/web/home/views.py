35be6e279a8cb30fd0ceaf00db77ad612851f6b3
Andrew Gein <andgein@yandex.ru>
2016-03-31 01:07:23 +0500

    Bugfix

--- src/web/home/views.py
+++ src/web/home/views.py
@@ -8,7 +8,7 @@ def home(request):
         return redirect('login')
 
     if not request.user.is_email_confirmed:
-        return redirect('user:complete')
+        return redirect('complete')
 
     current_school = school.models.School.objects.last()
     return redirect('school:index', school_name=current_school.short_name)

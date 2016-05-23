8594c47a36868d0444aa0ebf5a81c1d9d737d585
Андрей Гейн < >
2016-05-23 13:10:04 +0500

    Reset block field values if hide it from questionnaire

--- src/web/questionnaire/static/questionnaire/js/show-conditions.js
+++ src/web/questionnaire/static/questionnaire/js/show-conditions.js
@@ -3,8 +3,14 @@ $(document).ready(function(){
         var checked_at_least_one = $options.filter(':visible:checked').length > 0;
         if (checked_at_least_one)
             $block.show(200);
-        else
-            $block.hide(200);
+        else {
+            $block.hide(200, function () {
+                $block.find('input[type=checkbox]').prop('checked', false);
+                $block.find('input[type=radio]').prop('checked', false);
+                $block.find('input[type=text]').val('');
+                $block.find('textarea').val('');
+            });
+        }
     }
 
     $.fn.showOnlyIfOptionChecked = function ($options) {

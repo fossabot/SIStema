ce40534806e49702e97abfae977ff92ee8a28bc4
Egor Zhdan <egor.zhdan@gmail.com>
2016-07-31 12:33:32 +0300

    Add admin entries for settings items

---
+++ src/web/sistema/admin.py
@@ -0,0 +1,20 @@
+from django.contrib import admin
+from sistema import models
+
+
+class SettingsItemAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'short_name',
+        'display_name',
+        'description',
+        'school',
+        'session'
+    )
+
+admin.site.register(models.IntegerSettingsItem, SettingsItemAdmin)
+admin.site.register(models.BigIntegerSettingsItem, SettingsItemAdmin)
+admin.site.register(models.DateSettingsItem, SettingsItemAdmin)
+admin.site.register(models.DateTimeSettingsItem, SettingsItemAdmin)
+admin.site.register(models.CharSettingsItem, SettingsItemAdmin)
+admin.site.register(models.TextSettingsItem, SettingsItemAdmin)

cc5bfe070e31ec6188626aa8c27ba366eaa2db5f
Egor Zhdan <egor.zhdan@gmail.com>
2016-07-31 11:46:21 +0300

    Add model for storing settings items

--- src/web/sistema/models.py
+++ src/web/sistema/models.py
@@ -1,4 +1,8 @@
 from django.conf import settings
+from polymorphic.models import PolymorphicModel
+from django.db import models
+
+from schools.models import School, Session
 
 
 class DjangoSettingsReference(object):
@@ -11,6 +15,7 @@ class DjangoSettingsReference(object):
     models.FilePathField(path=DjangoSettingsReference('UPLOAD_DIR')). It will be saved in migration as
         models.FilePathField(path=DjangoSettingsReference('UPLOAD_DIR'))
     """
+
     def __init__(self, name):
         self.name = name
 
@@ -19,3 +24,54 @@ class DjangoSettingsReference(object):
 
     def deconstruct(self):
         return "%s.%s" % (__name__, self.__class__.__name__), (self.name,), {}
+
+
+class SettingsItem(PolymorphicModel):
+    name = models.CharField(max_length=100)
+
+    display_name = models.CharField(max_length=100)
+
+    description = models.TextField(blank=True)
+
+    school = models.ForeignKey(School, null=True, blank=True, default=None)
+
+    session = models.ForeignKey(Session, null=True, blank=True, default=None)
+
+    def save(self, *args, **kwargs):
+        if self.school is not None and self.session is not None:
+            if self.school_id != self.session.school_id:
+                raise ValueError("sistema.models: session field value contadicts school field value")
+
+        super().save()
+
+
+class IntegerSettingsItem(SettingsItem):
+    value = models.IntegerField()
+
+
+class BigIntegerSettingsItem(SettingsItem):
+    value = models.BigIntegerField()
+
+
+class PositiveIntegerSettingsItem(SettingsItem):
+    value = models.PositiveIntegerField()
+
+
+class TextSettingsItem(SettingsItem):
+    value = models.TextField()
+
+
+class CharSettingsItem(SettingsItem):
+    value = models.CharField(max_length=256)
+
+
+class EmailSettingsItem(SettingsItem):
+    value = models.EmailField()
+
+
+class DateTimeSettingsItem(SettingsItem):
+    value = models.DateTimeField()
+
+
+class DateSettingsItem(SettingsItem):
+    value = models.DateField()

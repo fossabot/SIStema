72405b1b474749f5460624a080589660b13a069f
ShaydurovaEkaterina <ka10a@mail.ru>
2016-07-31 12:41:18 +0300

    Add Admin Interface for PersonalEmail

--- src/web/modules/mail/admin.py
+++ src/web/modules/mail/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+from . import models
+
+
+class PersonalEmailAdmin(admin.ModelAdmin):
+    list_display = ('id', 'email-name', 'hash', 'is_active', 'owner', 'sessions')
+
+
+admin.site.register(models.PersonalEmail, PersonalEmailAdmin)
+
+

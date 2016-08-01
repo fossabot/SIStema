ee008abd636136c0fe4c679ef9c279ff922e21ae
Andrey Chulkov <achulkov2@gmail.com>
2016-08-01 09:55:16 +0300

    Configure urls for /mail/ (main inbox)

--- src/web/sistema/urls.py
+++ src/web/sistema/urls.py
@@ -10,4 +10,5 @@ urlpatterns = [
     url(r'^frontend/', include('frontend.urls', namespace='frontend')),
     url(r'^(?P<school_name>[^/]+)/', include('schools.urls', namespace='school')),
     url(r'^hijack/', include('hijack.urls')),
+    url(r'^mail/', include('modules.mail.urls', namespace='mail')),
 ]

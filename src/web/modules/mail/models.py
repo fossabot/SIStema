6e9ff1b411fa37654a7531b00a674b9c09fc8118
Vitaly Prishepa <gely902@gmail.com>
2016-08-01 08:38:08 +0300

    Add some string representations

--- src/web/modules/mail/models.py
+++ src/web/modules/mail/models.py
@@ -64,8 +64,14 @@ class EmailMessage(models.Model):
 class ContactList(models.Model):
     owner = models.ForeignKey(SisEmailUser)
 
+    def __str__(self):
+        return 'List of user ' + self.owner.__str__()
+
 
 class ContactRecord(models.Model):
     contact_list = models.ForeignKey(ContactList, related_name='contacts')
 
     person = models.ForeignKey(EmailUser)
+
+    def __str__(self):
+        return self.person.__str__()

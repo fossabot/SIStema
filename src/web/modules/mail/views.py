f515a2008a2d6bb38f515c44c1b0d46201e3f673
Andrey Chulkov <achulkov2@gmail.com>
2016-08-01 11:43:43 +0300

    Add views for mail user inbox

--- src/web/modules/mail/views.py
+++ src/web/modules/mail/views.py
@@ -1,5 +1,20 @@
+from django.contrib.auth.decorators import login_required
+from django.db.models import Q
 from django.shortcuts import render
 
+from . import models
 
+
+@login_required
 def inbox(request):
-    return None
+    ordered_inbox_email_list = models.EmailMessage.objects.filter(
+        Q(recipients__sisemailuser__user=request.user) |
+        Q(cc_recipients__sisemailuser__user=request.user)
+    ).order_by('-created_at')
+
+    return render(request, 'mail/inbox.html', {
+        'ordered_inbox_email_list': ordered_inbox_email_list,
+    })
+
+
+

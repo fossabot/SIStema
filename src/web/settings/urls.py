b2a11dfdb89fc066a85e2708b491116686c084ff
Egor Zhdan <egor.zhdan@gmail.com>
2016-08-04 12:59:17 +0300

    Improve template & remove useless urls

--- src/web/settings/urls.py
+++ src/web/settings/urls.py
@@ -4,6 +4,4 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.global_settings_list, name='index'),
-    url(r'^(?P<school_name>[^/]+)/$', views.school_settings_list, name='index'),
-    url(r'^(?P<school_name>[^/]+)/(?P<session_name>[^/]+)/$', views.session_settings_list, name='index')
 ]

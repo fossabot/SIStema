61de70ca5a3d67e6a516e67b8d7edc9058389c18
ShaydurovaEkaterina <ka10a@mail.ru>
2016-08-04 12:41:22 +0300

    Use random.sample in generate_text #10065 Extract printing new email and generating new ExternalEmailUser #10078

--- src/web/modules/mail/management/commands/generate_mail.py
+++ src/web/modules/mail/management/commands/generate_mail.py
@@ -1,4 +1,4 @@
-from random import choice, shuffle, randint
+from random import choice, shuffle, randint, sample
 import string
 
 from django.core.management.base import BaseCommand
@@ -19,7 +19,7 @@ def generate_display_name():
 
 def generate_login():
     logins = ['ben', 'max', 'ivan', 'kate', 'alex', 'tina', 'andrew', 'michael',
-              'serg','leo', 'polly', 'egor', 'den', 'ann', 'stas']
+              'serg', 'leo', 'polly', 'egor', 'den', 'ann', 'stas']
     return choice(logins)
 
 
@@ -42,11 +42,7 @@ def generate_text():
                'I will be at the party.', 'Have you ever seen this film?', 'Please, help me!',
                'London is the capital of Great Britain.', 'You should play the piano.',
                'I care what you think.', 'We are stressed out.']
-    shuffle(phrases)
-    text = ''
-    for i in range(randint(5, 10)):
-        text += phrases[i] + ' '
-    return text
+    return ' '.join(map(str, sample(phrases, randint(5, 10))))
 
 
 def generate_date():
@@ -61,55 +57,59 @@ class Command(BaseCommand):
         parser.add_argument('count_cc_recipients', type=int)
 
     def handle(self, *args, **options):
-        email_user_id = options['email_user_id']
-
-        if 'sender_id' in options.keys():
-            sender_id = options['sender']
+        #Find sender
+        if 'sender_id' in options:
+            sender_id = options['sender_id']
             try:
                 sender = EmailUser.objects.set(id=sender_id)
             except ObjectDoesNotExist:
                 print('Error: Given sender does not exist.')
                 return
         else:
-            login = generate_login()
-            domain = generate_domain()
-            while models.ExternalEmailUser.objects.filter(email=login + domain).exists():
-                for letter_index in range(randint(1, 5)):
-                    login = login + choice(string.ascii_lowercase + string.digits)
-            sender = models.ExternalEmailUser(display_name=generate_display_name(), email=login + domain)
-            sender.save()
+            sender = self.generate_external_email_user()
 
-        new_email = models.EmailMessage(sender=sender, subject=generate_subject(),
-                                        html_text=generate_text(), created_at=generate_date())
-        new_email.save()
-
-        for recipient_index in range(options['count_cc_recipients']):
-            login = generate_login()
-            domain = generate_domain()
-            while models.ExternalEmailUser.objects.filter(email=login + domain).exists():
-                for letter_index in range(randint(1, 5)):
-                    login = login + choice(string.ascii_lowercase + string.digits)
-            cc_recipient = models.ExternalEmailUser(display_name=generate_display_name(),
-                                                    email=login + domain)
-            cc_recipient.save()
-            new_email.cc_recipients.add(cc_recipient)
-
+        #Find recipient
+        email_user_id = options['email_user_id']
         try:
             recipient = EmailUser.objects.get(id=email_user_id)
         except ObjectDoesNotExist:
             print('Error: Given recipient does not exist.')
             return
 
+        new_email = models.EmailMessage(
+            sender=sender,
+            subject=generate_subject(),
+            html_text=generate_text(),
+            created_at=generate_date()
+        )
+        new_email.save()
+
         new_email.recipients.add(recipient)
         new_email.save()
 
+        #Add cc_recipients
+        for cc_recipient_index in range(options['count_cc_recipients']):
+            new_email.cc_recipients.add(self.generate_external_email_user())
+            new_email.save()
+
+        self.show_email(new_email)
+
+    def generate_external_email_user(self):
+        login = generate_login()
+        domain = generate_domain()
+        while models.ExternalEmailUser.objects.filter(email=login + domain).exists():
+            login = login + choice(string.ascii_lowercase + string.digits)
+        cc_recipient = models.ExternalEmailUser(display_name=generate_display_name(),
+                                                email=login + domain)
+        cc_recipient.save()
+        return cc_recipient
+
+    def show_email(self, new_email):
         print('Generation is successful.')
         print('From:', str(new_email.sender))
-        print('To:', str(recipient))
-        if options['count_cc_recipients']:
-            print('CC_Recipients:')
-            for user in new_email.cc_recipients.all():
-                print('    ', str(user))
+        print('To:', ' '.join(map(str, new_email.recipients.all())))
+        if len(new_email.cc_recipients.all()):
+            print('CC_Recipients:', ' '.join(map(str, new_email.cc_recipients.all())))
         print('Subject:', new_email.subject)
         print('Text:', new_email.html_text)
         print('Created at', str(new_email.created_at))

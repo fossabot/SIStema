964ce2049d64d35de488dd3cd5ba9b5360cf1e69
Egor Zhdan <egor.zhdan@gmail.com>
2016-08-05 08:32:54 +0300

    Rename functions #10086

--- src/web/settings/views.py
+++ src/web/settings/views.py
@@ -24,12 +24,12 @@ def groups_list_from_settings_list(settings):
     return apps
 
 
-def get_global():
+def get_global_settings():
     return list(models.SettingsItem.objects.filter(school__isnull=True, session__isnull=True))
 
 
-def get_school(school_name):
-    result = get_global()
+def get_school_settings(school_name):
+    result = get_global_settings()
     for item in list(models.SettingsItem.objects.filter(school__short_name=school_name, session__isnull=True)):
         for existing_item in result:
             if existing_item.short_name == item.short_name:
@@ -38,8 +38,8 @@ def get_school(school_name):
     return result
 
 
-def get_session(school_name, session_name):
-    result = get_school(school_name)
+def get_session_settings(school_name, session_name):
+    result = get_school_settings(school_name)
     for item in list(models.SettingsItem.objects.filter(school__isnull=True,
                                                         session__short_name=session_name,
                                                         session__school__short_name=school_name)):
@@ -52,14 +52,14 @@ def get_session(school_name, session_name):
 
 @sistema.staff.only_staff
 def global_settings_list(request):
-    settings_list = get_global()
+    settings_list = get_global_settings()
     groups = groups_list_from_settings_list(settings_list)
     return render(request, 'settings/list.html', {'apps': groups, 'area': 'global'})
 
 
 @sistema.staff.only_staff
 def school_settings_list(request, school_name):
-    settings_list = get_school(school_name)
+    settings_list = get_school_settings(school_name)
     groups = groups_list_from_settings_list(settings_list)
     school = get_object_or_404(School, short_name=school_name)
     return render(request, 'settings/list.html', {'apps': groups, 'area': 'school', 'name': school.name})
@@ -67,7 +67,7 @@ def school_settings_list(request, school_name):
 
 @sistema.staff.only_staff
 def session_settings_list(request, school_name, session_name):
-    settings_list = get_session(school_name, session_name)
+    settings_list = get_session_settings(school_name, session_name)
     groups = groups_list_from_settings_list(settings_list)
     school = get_object_or_404(School, short_name=school_name)
     session = get_object_or_404(Session, school=school, short_name=session_name)

03d9badc5a60d2eb2bd24fdbbda49a38681e158c
Vitaly Prishepa <gely902@gmail.com>
2016-08-06 09:12:03 +0300

    Add fucntion to save email in db #10125

--- src/web/modules/mail/urls.py
+++ src/web/modules/mail/urls.py
@@ -5,6 +5,5 @@ urlpatterns = [
     url(r'^$', views.inbox, name='inbox'),
     url(r'^compose/', views.compose, name='compose'),
     url(r'^contacts/', views.contacts, name='contacts'),
-    url(r'^send/', views.send_email, name='send'),
     url(r'^(?P<message_id>[^/]+)/', views.message, name='message'),
 ]

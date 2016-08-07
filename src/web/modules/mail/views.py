3e8000207e41e0341645e872d334ca47478c316b
Vitaly Prishepa <gely902@gmail.com>
2016-08-07 09:55:07 +0300

    1) Refactor function _save_email (change query) #10145 2) Add DOMAIN_MAIL in setting.py #10146

--- src/web/modules/mail/views.py
+++ src/web/modules/mail/views.py
@@ -11,27 +11,27 @@ from django.views.decorators.http import require_POST
 from django.utils.html import strip_tags
 
 from . import models, forms
+import sistema.settings
 
 
 # Parse recipients from string like: a@mail.ru, b@mail.ru, ...
 def _get_recipients(string_with_recipients):
     recipients = []
     for recipient in string_with_recipients.split(', '):
-        if models.EmailUser.objects.filter(
-                        Q(sisemailuser__user__email__iexact=recipient) |
-                        Q(externalemailuser__email__iexact=recipient)
-        ).exists():
-            recipients.append(models.EmailUser.objects.get(
-                Q(sisemailuser__user__email__iexact=recipient) |
-                Q(externalemailuser__email__iexact=recipient)
-            ))
+        if recipient == '':
+            continue
+        query = models.EmailUser.objects.filter(
+            Q(sisemailuser__user__email__iexact=recipient) |
+            Q(externalemailuser__email__iexact=recipient)
+        )
+        if query.first() is not None:
+            recipients.append(query.first())
         else:
-            if models.PersonalEmail.objects.annotate(
-                    full_email=Concat('email_name', Value('-'), 'hash', Value('@sistema.lksh.ru'),
-                                      output_field=TextField())).filter(full_email__iexact=recipient).exists():
-                recipients.append(models.PersonalEmail.objects.annotate(
-                    full_email=Concat('email_name', Value('-'), 'hash', Value('@sistema.lksh.ru'),
-                                      output_field=TextField())).get(full_email__iexact=recipient).owner)
+            query = models.PersonalEmail.objects.annotate(
+                    full_email=Concat('email_name', Value('-'), 'hash', Value(sistema.settings.MAIL_DOMAIN),
+                                      output_field=TextField())).filter(full_email__iexact=recipient)
+            if query.first() is not None:
+                recipients.append(query.first().owner)
             else:
                 external_user = models.ExternalEmailUser()
                 external_user.email = recipient
@@ -142,18 +142,6 @@ def message(request, message_id):
         'email': email,
         'allow_replying': is_recipient_of_email(request.user, email),
     })
-
-
-@login_required
-def send_email(request):
-    if request.method == 'POST':
-        form = forms.ComposeForm(request.POST)
-        if form.is_valid():
-            return JsonResponse({'result': 'ok'})
-        else:
-            return JsonResponse({'result': 'fail'})
-    return JsonResponse({'error': 'bad method'})
-
 
 MAX_STRING_LENGTH = 70
 

31b80c07a90c4c0807f29104c8ec69fb4c9400b3
Seva Zhidkov <zhidkovseva@gmail.com>
2016-08-11 17:08:12 +0300

    Fix from field in manage.py task to send test mail

--- src/web/modules/mail/management/commands/send_test_mail.py
+++ src/web/modules/mail/management/commands/send_test_mail.py
@@ -1,5 +1,6 @@
 from django.core.management.base import BaseCommand
 from django.core.mail import send_mail
+from anymail import exceptions
 
 TEST_MAIL_SUBJECT = "Do you know Taylor Swift?"
 TEST_MAIL_TEXT = """You said, "I remember how we felt sitting by the water.
@@ -7,7 +8,7 @@ And every time I look at you, it’s like the first time.
 I fell in love with a careless man’s careful daughter.
 She is the best thing that’s ever been mine."
 """
-TEST_MAIL_SENDER = "Anymail Sender <taylor@lksh.ru>"
+TEST_MAIL_SENDER = "Anymail Sender <taylor@mg.sistema.lksh.ru>"
 
 
 class Command(BaseCommand):
@@ -17,6 +18,9 @@ class Command(BaseCommand):
         parser.add_argument('recipient', nargs='+', type=str)
 
     def handle(self, *args, **options):
-        send_mail(TEST_MAIL_SUBJECT, TEST_MAIL_TEXT, TEST_MAIL_SENDER, [options['recipient']])
-        self.stdout.write(self.style.SUCCESS('Successfully closed poll "%s"'))
+        try:
+            send_mail(TEST_MAIL_SUBJECT, TEST_MAIL_TEXT, TEST_MAIL_SENDER, [options['recipient']])
+        except exceptions.AnymailAPIError as error:
+            print(error.response.text)
+        self.stdout.write(self.style.SUCCESS('Message sent'))
 

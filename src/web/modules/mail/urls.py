2f317e1e423d1477536a498f3daa28da209c9b05
Seva Zhidkov <zhidkovseva@gmail.com>
2016-08-11 10:31:18 +0300

    Add webhook template for Mailgun incoming emails

--- src/web/modules/mail/urls.py
+++ src/web/modules/mail/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     url(r'^compose/', views.compose, name='compose'),
     url(r'^contacts/', views.contacts, name='contacts'),
     url(r'^sent/', views.sent, name='sent'),
+    url(r'^webhook/', views.incoming_webhook),
     url(r'^attachment/(?P<attachment_id>[^/]+)/', views.download_attachment, name='download_attachment'),
     url(r'^(?P<message_id>[^/]+)/$', views.message, name='message'),
     url(r'^(?P<message_id>[^/]+)/delete/', views.delete_email, name='delete'),

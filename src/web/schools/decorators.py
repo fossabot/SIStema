3d0c3f1b07f8005b9a771acaf1abb3ec3fba4374
Artem Tabolin <artemtab@yandex.ru>
2016-12-22 21:57:13 +0100

    Add TODO to move school_name argument from sistema.urls to schools.urls

--- src/web/schools/decorators.py
+++ src/web/schools/decorators.py
@@ -3,6 +3,9 @@ from django import shortcuts
 from .models import *
 
 
+# TODO(Artem Tabolin): move the definition of school_name argument from
+#     sistema.urls to schools.url. The dependency on sistema app is not required
+#     here, so it should be removed.
 def school_view(view):
     def func_wrapper(request, school_name, *args, **kwargs):
         request.school = shortcuts.get_object_or_404(School,

20d701f806cc77bbd34585a0a255c94dbe332c61
Artem Tabolin <artemtab@yandex.ru>
2017-01-14 20:57:25 +0100

    Quick fix for deciding which school is the current one.
    Get the current school based on CURRENT_SCHOOL_SHORT_NAME variable value
    in settings.

--- src/web/home/views.py
+++ src/web/home/views.py
@@ -1,17 +1,19 @@
-from django.shortcuts import render, redirect
+from django import conf
+from django import shortcuts
 
 import schools.models
 
 
 def home(request):
     if not request.user.is_authenticated():
-        return redirect('users:login')
+        return shortcuts.redirect('users:login')
 
     if not request.user.is_email_confirmed:
-        return redirect('users:complete')
+        return shortcuts.redirect('users:complete')
 
-    # TODO(Artem Tabolin): That's the wrong way to get current school. I think
-    #    it should be explicitly set by admins.
-    current_school = schools.models.School.objects.last()
+    # TODO(Artem Tabolin): That's the wrong way to get the current school. We
+    #     should introduce some global settings module to hold its value.
+    short_name = conf.settings.CURRENT_SCHOOL_SHORT_NAME
+    current_school = schools.models.School.objects.get(short_name=short_name)
 
-    return redirect(current_school)
+    return shortcuts.redirect(current_school)

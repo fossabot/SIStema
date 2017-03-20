091bf67139ba136f1ab5d83e697b8b396eec2898
Eugene Kurpilyansky <dembelz@yandex.ru>
2017-03-20 13:02:46 +0300

    fix bug in UserProfileMiddleware (for staff user)

--- src/web/users/middleware.py
+++ src/web/users/middleware.py
@@ -5,8 +5,6 @@ class UserProfileMiddleware(object):
     def process_view(self, request, view_func, view_args, view_kwargs):
         request.user_profile = None
         if request.user.is_authenticated:
-            if request.user.is_staff:
-                return None
             if hasattr(request.user, 'user_profile'):
                 request.user_profile = request.user.user_profile
             else:

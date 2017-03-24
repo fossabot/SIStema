c03ed92719c663b493b1802484a4de7c5826cf37
Artem Tabolin <artemtab@yandex.ru>
2017-03-24 11:03:30 +0100

    Get rid of unnecessary .each in post-request-links.js

--- src/web/static/js/post-request-links.js
+++ src/web/static/js/post-request-links.js
@@ -1,19 +1,16 @@
 /* Enclose links with post-request-link css class*/
 $(document).ready(function() {
-    $('.post-request-link[href]').each(function(_, link) {
-      $(link).click(function(e) {
-        var $this = $(this);
-        e.stopPropagation();
-        e.preventDefault();
+    $('.post-request-link[href]').click(function(e) {
+      var $this = $(this);
+      e.stopPropagation();
+      e.preventDefault();
 
-        var $form = $('<form>');
-        $form.attr('method', 'POST').attr('action', $this.attr('href')).hide();
-        $('<input>').attr('type', 'hidden')
-                    .attr('name', 'csrfmiddlewaretoken')
-                    .attr('value', Cookies.get('csrftoken'))
-                    .appendTo($form);
-        $form.appendTo('body').submit();
-      });
-      
+      var $form = $('<form>');
+      $form.attr('method', 'POST').attr('action', $this.attr('href')).hide();
+      $('<input>').attr('type', 'hidden')
+                  .attr('name', 'csrfmiddlewaretoken')
+                  .attr('value', Cookies.get('csrftoken'))
+                  .appendTo($form);
+      $form.appendTo('body').submit();
     });
 });

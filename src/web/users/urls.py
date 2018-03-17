f6216437d64a5f877737779a90a2e4ad2bdc9503
Artem Tabolin <artemtab@yandex.ru>
2018-03-17 22:30:37 +0100

    Remove custom autocomplete view from users

--- src/web/users/urls.py
+++ src/web/users/urls.py
@@ -2,16 +2,9 @@ from django.conf.urls import url, include
 
 from users import views
 
-admin_urlpatterns = [
-    url(r'^user-autocomplete/$',
-        views.UserAutocomplete.as_view(),
-        name='user-autocomplete'),
-]
-
 urlpatterns = [
     url(r'^accounts/settings/$', views.account_settings, name='account_settings'),
     url(r'^accounts/find-similar/$', views.find_similar_accounts, name='account_find_similar'),
     url(r'^accounts/', include('allauth.urls')),
     url(r'^user/profile$', views.profile, name='user-profile'),
-    url(r'^users/admin/', include(admin_urlpatterns)),
 ]

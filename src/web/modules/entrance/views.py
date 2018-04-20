9e6a94a417ba8cd2e03d17b45c2213ef46829a7e
Artem Tabolin <artemtab@yandex.ru>
2018-04-20 19:18:38 +0200

    Fix bug with not showing user solutions

--- src/web/modules/entrance/views.py
+++ src/web/modules/entrance/views.py
@@ -153,6 +153,8 @@ def exam(request, selected_task_id=None):
     # Order task by type and order
     tasks = sorted(tasks, key=lambda t: (t.type_title, t.order))
     for task in tasks:
+        task.user_solutions = list(
+            task.solutions.filter(user=request.user).order_by('-created_at'))
         task.is_accepted = task.is_accepted_for_user(request.user)
         task.is_solved = task.is_solved_by_user(request.user)
         task.form = task.get_form_for_user(request.user)

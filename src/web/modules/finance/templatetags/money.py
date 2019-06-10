b6412496d789f20c11fbe9c56d8e7c6192100172
Andrew Gein <andgein@yandex.ru>
2019-06-11 01:05:47 +0500

    Call `pluralize` instead of `russian_pluralize`

--- src/web/modules/finance/templatetags/money.py
+++ src/web/modules/finance/templatetags/money.py
@@ -1,7 +1,7 @@
 from django.template import Library
 
 from modules.finance.models.currency import Currency
-from sistema.templatetags.number_to_text import russian_pluralize
+from sistema.templatetags.number_to_text import pluralize
 
 register = Library()
 
@@ -16,4 +16,4 @@ def money_russian_pluralize(amount, currency):
         Currency.EUR: ('евро', 'евро', 'евро'),
         Currency.USD: ('доллар', 'доллара', 'долларов'),
     }
-    return str(amount) + ' ' + russian_pluralize(amount, forms[currency])
+    return str(amount) + ' ' + pluralize(amount, *forms[currency])
